72f255dbbc49f6478de7e716f78883329a67d6b8
David Pierce <david.dean.pierce@gmail.com>
2012-03-01 12:44:45 -0800

    Passes the spec that requires child, service, and classroom objects to be accessible through associations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -10,9 +10,9 @@
 #  updated_at   :datetime
 #
 class Attendance < ActiveRecord::Base
-#  has_one :service
-#  has_one :classroom
-#  has_one :child
+  has_one :service
+  has_one :classroom
+  has_one :child
 
   validates_presence_of :service_id, :classroom_id, :child_id
 
