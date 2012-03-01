33b7ddaf58a5c4e3531bdca129af8843256eb792
David Pierce <david.dean.pierce@gmail.com>
2012-03-01 12:45:38 -0800

    Getting a start on the home page spec

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe "HomePages" do
-  describe "GET /home_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get home_pages_path
-      response.status.should be(200)
-    end
+  describe "Home page" do
+    before { visit root_path }
+    it { should contain_text 'Pick a class to list the students' }
   end
 end
