7ba3bdd533b63cdcf780ba37e9309cbe664c7ef5
David Pierce <david.dean.pierce@gmail.com>
2012-03-03 12:28:11 -0800

    Add test for authentication checking

--- spec/requests/home_pages_spec.rb
+++ spec/requests/home_pages_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-describe "HomePages" do
+describe "Home Page" do
   subject { page }
+  let(:volunteer) { FactoryGirl.create(:volunteer) }
 
-  describe "Home page" do
-    let(:volunteer) { FactoryGirl.create(:volunteer) }
-    
-    before do 
-      visit root_path 
-      sign_in(volunteer)
-    end
+  before { visit root_path }
+  describe "requires authentication" do
+    it { should have_selector('h2', :text => 'Sign in') } 
+  end
 
+  describe "as signed in volunteer" do
+    before { sign_in(volunteer) }
     it { should have_selector('p', :text => 'Pick a class to list the students') }
   end
 end
