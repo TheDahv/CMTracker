33634352845778e0ee6b9605b634a9155749ec3d
David Pierce <david.dean.pierce@gmail.com>
2012-03-04 18:46:37 -0800

    Seed data for tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -5,25 +5,38 @@ def sign_in(volunteer)
 end
 
 def seed
-  make_classroom("Nursery")
-
-  make_classroom("Toddler1")
-
-  make_classroom("Toddler2")
-
-  make_classroom("456")
-
-  make_classroom("7+")
-
-  c = make_classroom("Inactive")
-  c.toggle!(:active)
+  make_classes
+  make_kids
+  make_services
 end
 
 private
-  def make_classroom(name) 
-    c = FactoryGirl.create(:classroom)
-    c.name =  name
-    c.active = true
+  def make_classes
+     %w{Nursery Toddler1 Toddler2 456 7+ Inactive}.each do |name|
+      make_classroom(name) 
+    end
+    Classroom.find_by_name('Inactive').toggle!(:active)
+  end
 
-    c
+  def make_kids
+    Classroom.all.each do |classroom|
+      10.times do
+        classroom.children.create( 
+          :first_name => Faker::Name.first_name,
+          :last_name => Faker::Name.last_name,
+          :inactive => false
+        ) 
+      end
+    end
+  end
+
+  def make_classroom(name) 
+    Classroom.create!(
+      :name => name,
+      :active => true
+    )
+  end
+
+  def make_services
+    s = Service.create!(:service_date => DateTime.civil(2012, 2, 24))
   end
