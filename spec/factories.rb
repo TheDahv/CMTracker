9baf0a11314c1f1b38c1690228add78869768ca7
David Pierce <david.dean.pierce@gmail.com>
2012-03-04 18:44:49 -0800

    Define more factory descriptions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :volunteer do
     first_name 'Test'
@@ -10,5 +12,17 @@ FactoryGirl.define do
 
   factory :classroom do
     name 'Classroom'
+  end
+
+  factory :child do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+  end
+
+  factory :service do
+    service_date Date.today
+  end
+
+  factory :attendance do
   end
 end
